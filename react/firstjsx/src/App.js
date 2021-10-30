da9b35350ffb7ed5753f2719bbebf0dd0bcdc184
sfoos1121 <sandiefoos@gmail.com>
2021-10-30 14:19:13 -0400

    adding first jsx

---
+++ react/firstjsx/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Get a new job</li>
+        <li>Hang out with my family</li>
+        <li>Game more</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
