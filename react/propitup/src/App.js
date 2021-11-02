c523350469df17c536a8038660ccc3682f047229
sfoos1121 <sandiefoos@gmail.com>
2021-11-01 21:08:24 -0400

    propritup

--- react/propitup/src/App.js
+++ react/propitup/src/App.js
@@ -5,10 +5,10 @@ import Person from './components/Person';
 function App() {
   return (
     <div className="App">
-      <Person name={"Doe, Jane"} age={"45"} hair={"Black"} />
-      <Person name={"Smith, John"} age={"88"} hair={"Brown"} />
-      <Person name={"Fillmore, Millard"} age={"45"} hair={"Brown"} />
-      <Person name={"Smith, Marla"} age={"62"} hair={"Brown"} />
+      <Person name={"Doe, Jane"} lastName={"Doe"} firstName={"Jane"} age={45} hair={"Black"} />
+      <Person name={"Smith, John"}lastName={"Smith"} firstName={"John"} age={88} hair={"Brown"} />
+      <Person name={"Fillmore, Millard"} lastName={"Fillmore"} firstName={"Millard"} age={45} hair={"Brown"} />
+      <Person name={"Smith, Marla"} lastName={"Smith"} firstName={"Marla"} age={62} hair={"Brown"} />
     </div>
   );
 }
--- react/propitup/src/components/Person.js
+++ react/propitup/src/components/Person.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Person = (props) => {
+    const { name, lastName, firstName, age, hair } = props;
+    const [ addBirthday, setAddBirthday ] = useState(age);
     return (
         <div>
-            <h1>{ props.name }</h1>
-            <p>Age: { props.age }</p>
-            <p>Hair Color: { props.hair} </p>
+            <h1>{ name }</h1>
+            <p>Age: { addBirthday }</p>
+            <p>Hair Color: { hair} </p>            
+            <button onClick={ (event) => setAddBirthday(addBirthday +1)}>Birthday Button for {firstName} {lastName}
+            </button>
         </div>
-    );
+    )
 }
 
 export default Person;
